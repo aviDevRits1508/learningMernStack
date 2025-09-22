ae84bc6a07c80a33fc034afccb8e705fdeea8973
Chaithanya <Chaitanya.P@revappayyaitservices.com>
2025-09-22 11:50:20 +0200

    first js file

---
+++ learningjs/class1.js
@@ -0,0 +1,28 @@
+console.log("Hello world")
+
+let name="Heyansha"
+console.log(name)
+
+const age=3
+console.log(age)
+
+name="chaithanya"
+console.log(name)
+
+// age=5
+console.log(age)
+
+// Var is a function scope
+
+var address="Bengaluru"   
+console.log(address)
+
+var address="Tumkur"      
+console.log(address)
+
+// let & const are the block scope 
+console.log(name)
+
+//  age=7
+console.log(age)
+
