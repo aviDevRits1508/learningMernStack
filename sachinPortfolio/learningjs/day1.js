ce451691a158e9b46cef1338dbc8d9daa3642d36
viquarqua <VIQUAR.QUADRI@revappayyaitservices.com>
2025-09-23 19:56:51 +0530

    last

--- sachinPortfolio/learningjs/day1.js
+++ sachinPortfolio/learningjs/day1.js
@@ -1,10 +1,31 @@
-console.log("Hi sachin");
-let name ="sachin";
-console.log(name);
-name = "solapure";
-console.log(name);
-const pin = "45";
-console.log(pin);
+// console.log("Hi sachin");
+// let name ="sachin";
+// console.log(name);
+// name = "solapure";
+// console.log(name);
+// const pin = "45";
+// console.log(pin);
 
-var car = "bmw";
-console.log(car);
+// var car = "bmw";
+// console.log(car);
+
+let book="about home";
+console.log(book);
+
+
+book = "about school";
+console.log(book);
+
+let book1 = "about phone";
+console.log(book1);
+
+const book2 = "about laptop";
+console.log(book2);
+// book2 = "about pen";
+// console.log(book2);
+var lm2 = "sachin";
+console.log(lm2);
+lm2 = "solapure";
+console.log(lm2);
+var lm2 = "siddeshwar";
+console.log(lm2);
