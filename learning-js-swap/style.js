05abbf329fdd472adef0771aaf26325b15cdab03
Pswapnita-11 <Swapnita.Patil@revappayyaitservices>
2025-09-24 00:21:08 +0530

    day-2-work/swapnita

---
+++ learning-js-swap/style.js
@@ -0,0 +1,41 @@
+console.log("Hello World"); 
+
+// Variable 
+
+let name="john";
+console.log(name);
+
+var lname="patil";
+console.log(lname);
+
+const Mname="swapnita";
+console.log(Mname);
+
+const a="23";
+console.log(a);
+
+name = "Doe";
+console.log(name);
+
+// let variables allow reassignment but const does not allow reassignment. 
+// Const have constant CSSMathValue, that means it cannot be changed.
+// var variable is older way of declaring variable. It is function scoped.can reassign values.
+
+// Datatypes in JS   ..(day-02)
+
+let age=23; //number
+let fname="swapnita"; //string
+let isApproved=true; //boolean
+let lastname; //undefined
+let selectedColor=null; //null
+let float=23.5; //float
+let bigint = 5n; //bigint
+let symbol = Symbol("id"); //symbol
+
+console.log(typeof age)
+console.log(typeof fname)
+console.log (typeof isApproved)
+console.log(typeof lastname)
+console.log(typeof selectedColor)
+console.log(typeof float)
+console.log(typeof bigint)
