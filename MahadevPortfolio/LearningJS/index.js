58783c1da7d02f93d92ab29b9bc6a8135ad2b9d0
Mahadev315 <Mahadev.k@revappayyaitservices.com>
2025-09-24 18:21:35 +0530

    Js Problems

--- MahadevPortfolio/LearningJS/index.js
+++ MahadevPortfolio/LearningJS/index.js
@@ -1,8 +1,5 @@
-console.log("Hello world")
 
-let name="Mahadev"
-console.log(name)
-
+// Total marks
 function totalMarks(social, science, maths, english, hindi) {
   return social + science + maths + english + hindi;
 }
@@ -12,3 +9,35 @@ let percentage = (total / 500) * 100;
 
 console.log("Percentage:" + percentage + "%");
 console.log("Total Marks:" + total);
+
+// Total marks
+function totalmarks(english, maths, kannada, hindi)
+{
+    let total=english+maths+kannada+hindi;
+    console.log(total)
+
+    let totals=(total/400)*100;
+    console.log(totals)
+}
+
+totalMarks(50,80,38,58);
+
+
+// Area of Triangle
+function areaoftriangle(base, height) {
+    return 0.5 * base * height;
+}
+
+console.log("area:", areaoftriangle(10,5));
+
+
+// Area of Circle
+function areaofCircle(radius) {
+    return Math.PI * radius * radius;
+}
+
+console.log("circle:",areaofCircle(7));
+
+
+
+
---
+++ MahadevPortfolio/LearningJS/index2.js
@@ -0,0 +1,4 @@
+console.log("Hello world")
+
+let name="Mahadev"
+console.log(name)
