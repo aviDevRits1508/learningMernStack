90f523778c9234ca8e9b39640f0131270a759e97
viquarqua <VIQUAR.QUADRI@revappayyaitservices.com>
2025-09-24 13:22:32 +0530

    day2

---
+++ ViquarsPortfolio/viquarjs/viquar1.js
@@ -0,0 +1,9 @@
+function totalMarks(social, science, maths, english, hindi) {
+  return social + science + maths + english + hindi;
+  
+}
+
+let total = totalMarks(85, 90, 78, 95, 88);
+let percentage = (total / 500) * 100;
+console.log("Percentage:", percentage + "%")
+console.log("Total Marks:", total);
