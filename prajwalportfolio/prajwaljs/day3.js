57264b8fa346976f90a50253639fa60fdc0245dc
prajwalrits <prajwal.m@revappayyaitservices.com>
2025-09-25 20:08:24 +0530

    added new file

---
+++ prajwalportfolio/prajwaljs/day3.js
@@ -0,0 +1,31 @@
+function operation(a,b){
+    if(a>b){
+        sub=a-b
+        console.log(`the Substraction of ${a} and ${b} is : ${sub}`)
+    }
+    else{
+        add=a+b
+        console.log(`the addition of ${a} And ${b} is : ${add}`)
+    }
+}
+operation(50,60)
+operation(60,40)
+
+// operator
+let a=20
+let b=50
+console.log(a+=b)
+
+
+// comparision operator
+let x=20
+let y="40"
+
+let z=(x==y)
+console.log(z)
+
+let f=(x===y)
+console.log(f)
+
+console.log(Number(x))
+
