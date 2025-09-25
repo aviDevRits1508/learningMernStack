fc538ed3416d5b152598b1108826551b5080b652
Mahadev315 <Mahadev.k@revappayyaitservices.com>
2025-09-26 00:51:43 +0530

    Assignment

---
+++ MahadevPortfolio/LearningJS/assign.js
@@ -0,0 +1,30 @@
+function isSick(a){
+    if(a){
+        console.log("not able to work")
+    }else{
+        console.log("able to work")
+    }
+}
+
+let anything = false;
+console.log(isSick(anything))
+
+
+let a=4
+let b=6
+if(a>b){ 
+    console.log(a+b)
+}else{
+    console.log(a-b)
+}
+
+
+function compare(x,y){
+    if(x>y){
+    console.log(x+y)
+}else{
+    console.log(x-y)
+}
+}
+let compare1 = compare(4,8);
+console.log(compare(compare1))
