d3e2f9182531f704380eb6e8eea92a984dc125cd
suvarna-patil454 <Suvarna.thalkari@revappayyaitservices.com>
2025-09-25 10:36:10 +0530

    adding changes changes in js

--- suvarnaPortfolio/dayOne.js
+++ suvarnaPortfolio/dayOne.js
@@ -24,4 +24,32 @@ a=8
 
 console.log(a)
 
+//functions
+function sum(a,b){
+    console.log("control1 came here")
+    return a+b
+}
+ 
+//execute the sum1
+let sum1 = (1000,2000)
+console.log(sum1)
 
+
+
+
+function mul(a,b){
+    console.log("control2 came here")
+    return a*b
+}
+
+//craete a one function whic is calculated the total makrs of student
+ function studentMarks(name,Hindi,English,kannada,Science,Socail,Maths){
+    //sum of all marks
+    let totalMarks = Hindi+English+kannada+Science+Socail+Maths;
+ 
+ //print result
+    console.log(`${name}'s total marks: ${totalMarks}`);
+ }
+ //call function
+   studentMarks("Rahul",95,85,98,88,95,90);
+ 
