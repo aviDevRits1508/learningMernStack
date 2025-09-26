77cfd6ec6ae42b440231169fcef13a943449ca7e
viquarqua <VIQUAR.QUADRI@revappayyaitservices.com>
2025-09-26 11:26:11 +0530

    day2

--- ViquarsPortfolio/viquarjs/viquar1.js
+++ ViquarsPortfolio/viquarjs/viquar1.js
@@ -1,3 +1,4 @@
+
 function totalMarks(social, science, maths, english, hindi) {
   return social + science + maths + english + hindi;
   
@@ -5,5 +6,38 @@ function totalMarks(social, science, maths, english, hindi) {
 
 let total = totalMarks(85, 90, 78, 95, 88);
 let percentage = (total / 500) * 100;
-console.log("Percentage:", percentage + "%")
+
 console.log("Total Marks:", total);
+console.log("Percentage:", percentage + "%")
+
+
+// day2
+
+function isSick(a){
+  if(a){
+    console.log()
+  }else{
+    console.log()
+  }
+}
+
+
+let a=4
+let b=10
+
+if (a>b){
+  console.log(a*b)
+}else{
+  console.log(a-b)
+}
+
+
+function compare(x,y){
+  if(x>y){
+    console.log(x+y)
+  }else{
+    console.log(x-y)
+  }
+}
+let compare = compare(4,7);
+console.log(compare(compare1))
