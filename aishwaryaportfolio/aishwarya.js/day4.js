7354ca6a2efde61109d714f69728752565a081fb
AishwaryaGumme <aishwarya.g@revappayyaitservices.com>
2025-10-03 18:08:51 +0530

    day4

---
+++ aishwaryaportfolio/aishwarya.js/day4.js
@@ -0,0 +1,30 @@
+for(let i=0; i <10; i++){
+    console.log(i + 1)
+}
+
+for(let i=1; i <= 10; i++) {
+    console.log(i * 2)
+}
+
+num = 2;
+function printTable(num){
+    for(let a=1; a<=10; a++ ) {
+        console.log(a*2)
+    }
+
+}
+
+
+for(let a=1; a<=20; a++){
+    console.log(a*5)
+}
+
+
+for(let a=1; a<10; a++){
+    console.log(a*3)
+}
+
+
+for(let a=10; a>=0; a--){
+    console.log(a);
+}
