51c7567636f2d0c6b2a6bf026cba06f33210f96f
Chaithanya <Chaitanya.P@revappayyaitservices.com>
2025-10-07 19:29:22 +0200

    add

---
+++ Webpage/chaithanya/LearningJs/day7.js
@@ -0,0 +1,120 @@
+// Print 1 to 10 using for loop
+
+// function printnumbers(a){
+//     for(let i=1;i<=a;i++){
+//         console.log(i);
+//     }
+// }
+
+// printnumbers(10)
+
+// Print 10 to 1 uing for loop
+// function printnumbers(a){
+//     for(let i=a;i>=1;i--){
+//         console.log(i)
+//     }
+// }  
+
+// printnumbers(10)
+
+// Sum of first 10 numbers
+// function numbers(a){
+//     let sum=0;
+//     for(let i=1;i<=a;i++){
+//         sum=sum+i;
+//         console.log(sum);
+//     }
+// }
+// numbers(10)
+
+// print all even numbers between 1 to 20
+// function even(a){
+//     let sum=0;
+//     for(let i=1;i<=a;i++){
+//         if(i%2==0){
+//             sum=sum+i;
+//             console.log(i);
+//         }
+//     }
+//     console.log(sum);
+// }  
+// even(20)
+
+// print reverse numbers from 10 to 1
+//    function reverse(a){
+//     for(i=a;i>=1;i--){
+//         console.log(i);
+//     }
+//    }
+//    reverse(10)
+
+// Sum of digits 
+
+
+    // let i=1;
+    // while(i<=5){
+    // console.log(i);
+    //  i++;
+    // }
+
+//  Sum of first 5 numbers using while loop
+//     function sumofdigits(a){
+//         let sum=0;
+//         i=1;
+//         while(i<=a){
+//             sum=sum+i;
+//             console.log(sum);
+//             i++;
+//         }
+//     }  
+
+// sumofdigits(20)
+
+// reverse numbers from 321 to 123
+// function reverse(a){
+//     while(a>=1){
+//         console.log(a);
+//         a--;
+//     }
+// }
+//    reverse(10)
+
+// print all odd numbers between 1 to 20
+// function odd(a){
+//     let sum=0;
+//     i=1;
+//     while(i<=a){
+//         if(i%2!=0){
+//             sum=sum+i;
+//             console.log(sum);
+//         }
+
+//             i++;
+       
+//     }
+// }
+// odd(20)
+
+
+// print 1 to N numbers using while loop
+// function numbers(a){
+//     let i=1;
+//     while(i<=a){
+//         console.log(i);
+//         i++;
+//     }
+// }
+// numbers(20)
+
+// Factorial of numbers using while loop
+function factorial(a){
+    let mul=1;
+    i=1;
+    while(i<=a){
+       mul=mul*i;
+       console.log(mul);
+       i++;
+}
+
+}
+factorial(5);
