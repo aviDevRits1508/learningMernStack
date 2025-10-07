8c98be12531867d757a406f95787d8e04408d753
viquarqua <VIQUAR.QUADRI@revappayyaitservices.com>
2025-10-07 15:34:46 +0530

    7oct

--- ViquarsPortfolio/viquarjs/viquar1.js
+++ ViquarsPortfolio/viquarjs/viquar1.js
@@ -169,12 +169,12 @@
 
 
 // 7 Sum of digits
-// let num = 12345;
+// let num = 1234;
 // let sum = 0;
 
 // while (num > 0) {
-//   sum += num % 4;
-//   num = (num / 4);
+//   sum += num % 10;
+//   num = (num / 10);
 // }
 
 // console.log("Sum of digits:", sum);
