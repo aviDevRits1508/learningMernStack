67b118bd2f5aac4b8b9c927069692b7b775e1893
Valli-dk <valli.dk@revappayyaitservices.com>
2025-10-08 11:02:31 +0530

    day 7

--- valliPortfolio/learningjs/day5.js
+++ valliPortfolio/learningjs/day5.js
@@ -16,15 +16,37 @@
 //     console.log("5*i=")
 // }
 
-let number = 5;
 
-console.log(`Table of ${number}:`);
+// 5th table
 
-for (let i = 1; i <= 10; i++) {
-  console.log(`${number} x ${i} = ${number * i}`);
-}
+// let number = 5;
+
+// console.log(`Table of ${number}:`);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${number} x ${i} = ${number * i}`);
+// }
+
+// function table(a) {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(a*i);
+//   }
+// }
+
+// table(5);
 
 
+// function even(a) {
+//     for (let i = 1; i <=a; i++) {
+//         console.log(i % 2 == 0 ? `even${i}` : `odd${i}`);
+//     }
+// }
+// even(10);
 
 
+// for (let i = 0; i < 5; i++) {
+//     console.log();
+//         for (let j = 1; j <= 4; j++) {  
+//             console.log(j); 
+//             }
+// }
 
