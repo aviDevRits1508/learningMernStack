291a8e679820ccb02bca88c38402a5924f30e836
Valli-dk <valli.dk@revappayyaitservices.com>
2025-10-08 11:02:18 +0530

    Day 7

---
+++ valliPortfolio/learningjs/day5.js
@@ -0,0 +1,30 @@
+// let result = "16"
+
+// switch (true) {
+//     case result >= 18:
+//         console.log("You can vote")
+//         break;
+//     case result < 18:
+//         console.log("Not eligible")
+//         break;
+//     default:
+//         console.log("Invalid entry")
+// }
+
+
+// for(i = 5; i <= 50; i++) {
+//     console.log("5*i=")
+// }
+
+let number = 5;
+
+console.log(`Table of ${number}:`);
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
+
+
+
+
