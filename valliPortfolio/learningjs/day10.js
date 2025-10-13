24cd33fac5cc951192e3411a684b061a7c996e18
Valli-dk <valli.dk@revappayyaitservices.com>
2025-10-13 18:08:36 +0530

    Updated

---
+++ valliPortfolio/learningjs/day10.js
@@ -0,0 +1,37 @@
+// for(let i = 1; i <= 2; i++){
+//     for(let j = 1; j <= 2; j++){
+//         for(let k = 1; k <= 10; k++){
+//             console.log("hello world")
+//         }
+//     }
+// }
+
+
+// let count = 0;
+
+// for(let i = 1; i < 2; i++){         // Outer loop → runs 1 time
+//     for(let j = 1; j < 2; j++){     // Second loop → runs 1 time per i
+//         for(let k = 1; k < 10; k++){// Third loop → runs 9 times per j
+//             for(let l = 1; l < 10; l++){ // Fourth loop → runs 9 times per k
+//             }
+//         }
+//     }
+// }
+
+// console.log(count);
+
+
+// let count = 0;
+
+// for(let i = 0; i < 2; i++){         // Outer loop → 2 times
+//     for(let j = 1; j < 2; j++){     // Second loop → 1 time per i
+//         for(let k = 1; k < 10; k++){// Third loop → 9 times per j
+//             for(let l = 1; l < 10; l++){ // Innermost loop → 9 times per k
+//                 count++;
+//             }
+//             console.log(`k iteration ${k}`, count);
+//         }
+//     }
+// }
+
+// console.log(count);
