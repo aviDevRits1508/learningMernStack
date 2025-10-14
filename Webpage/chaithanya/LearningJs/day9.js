26893f3c16905461b755b42d5a0904c4c2447191
Chaithanya <Chaitanya.P@revappayyaitservices.com>
2025-10-14 18:24:01 +0200

    add changes

---
+++ Webpage/chaithanya/LearningJs/day9.js
@@ -0,0 +1,88 @@
+// let count=0;
+// for(let i = 1; i <= 5; i++){
+//     for(let j = 1; j <= 4; j++){    
+//         for(let k = 1; k <= 3; k++){
+//             for(let l = 1; l <= 2; l++){
+//              count=count+1;
+             
+//             }
+//         }
+//     }
+// }
+ 
+//
+
+let number= [1,2,3,4,5];
+// let ans=new Array(number.length);
+
+// function reversearray(arr,ans){
+//     for(let i=1;i<=arr.length;i++){
+//         ans[i]=arr[i]*2;
+//     }
+//     return ans;
+// }
+
+// console.log(reversearray(number,ans));
+
+// let num =[1,2,3,4,5];
+// let key=3;
+// function searcharray(arr,key){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]===key){
+//             return i;
+//         }
+//     }
+// }
+
+// console.log(searcharray(num,key));
+
+// let arr=[1,2,3,4,5];
+// let sum=0;
+
+// function sumofarray(arr){
+//     for(i=0;i<arr.length;i++){  //1+3+6+10+15
+//         sum=sum+arr[i];
+//     }
+//     return sum;
+// }
+
+// console.log(sumofarray(arr));
+
+//  Minimum number in array
+// let arr=[5,4,3,2,1];
+// let min=arr[0];
+
+// function minofarray(arr){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]<min){
+//             min=arr[i];
+//         }
+//     }
+//     return min;
+// }
+
+// console.log(minofarray(arr));
+
+// Maximum number in array
+// let arr=[5,4,3,8,1];
+// let max=arr[0];
+
+// function maxofarray(arr){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>max){
+//             max=arr[i];
+
+//         }  
+//     }
+//     return max;
+// }
+//  console.log(maxofarray(arr));
+
+// Reverse an array
+let arr=[1,2,3,4,5];
+let start=0;
+let end=arr.length-1;
+
+function reversearray(arr){
+    
+}
