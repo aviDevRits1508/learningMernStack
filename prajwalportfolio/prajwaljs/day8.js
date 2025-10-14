0c10dd70673eb02252054e612bc6d23b36cbb421
prajwalrits <prajwal.mrevappayyaitservices.com>
2025-10-14 20:08:13 +0530

    added new file

--- prajwalportfolio/prajwaljs/day8.js
+++ prajwalportfolio/prajwaljs/day8.js
@@ -57,6 +57,56 @@
 // }
 // console.log(num)
 
+
+
+// // 1. Uppercase Conversion
+// let str1 = "hello world";
+// console.log("Uppercase:", str1.toUpperCase());
+
+// // 2. Lowercase Conversion
+// let str2 = "HELLO WORLD";
+// console.log("Lowercase:", str2.toLowerCase());
+
+// // 3. Find Word in Sentence
+// str1="my name is prajwal"
+// console.log(str1.includes("prajwal"))
+
+// // 4. Find Word Position
+// let sentence1 = "Hello world";
+// console.log("Position of 'world':", sentence1.indexOf("world")); 
+
+// // 5. Extract Substring
+// let lang = "JavaScript";
+// console.log("Extracted Substring:", lang.slice(4));
+// function slice(a){
+//     console.log(a.slice(4,10))
+// }
+// slice("javascript")
+
+// // 6. Replace a Word
+// let nameStr = "My name is John";
+// console.log("After Replace:", nameStr.replace("John", "Prajwal"));
+
+// // 7. Count String Length
+// let message = "I love coding";
+// console.log("Length:", message.length);
+
+// // 8. Combine Two Strings
+// let str3 = "Hello";
+// let str4 = "World";
+// console.log("Combined:",str3.concat(" ",str4));
+
+// // 9. Check Start or End Word
+// let str5 = "Hello JavaScript";
+// console.log("Starts with 'Hello':", str5.slice(0,5));
+// console.log("Ends with 'Script':", str5.slice(10,16));
+
+// 10. Reverse a String
+function reverseString(str) {
+   console.log(str.split("").reverse().join(""));
+}
+reverseString("Prajwal")
+
 // let count = 0;
 // for(let i = 1; i <= 2; i++){
 //     for(let j = 1; j <= 2; j++){
@@ -112,6 +162,82 @@ while (y < 5) {
     y++;
 }
 
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
+
+let i = 1;
+while (i <= 4) {
+    let j = 1;
+    let line = "";
+    while (j <= 4) {
+        line += j + " ";
+        j++;
+    }
+    console.log(line.trim());
+    i++;
+}
+
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+
+let a = 1;
+let num = 1;
+while (a <= 5) {
+    let b = 1;
+    let row = "";
+    while (b <= a) {
+        row += num + " ";
+        num++;
+        b++;
+    }
+    console.log(row.trim());
+    a++;
+}
+
+// 10 9 8 7 6 5 4 3 2 1
+// 10 9 8 7 6 5 4 3 2 1
+
+let x = 1;
+while (x <= 2) {
+    let y = 10;
+    let row = "";
+    while (y >= 1) {
+        row += y + " ";
+        y--;
+    }
+    console.log(row.trim());
+    x++;
+}
+
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+let m = 5;
+while (m >= 1) {
+    let n = 1;
+    let line = "";
+    while (n <= m) {
+        line += n + " ";
+        n++;
+    }
+    console.log(line.trim());
+    m--;
+}
+
+
+
 
 
 
@@ -119,52 +245,5 @@ while (y < 5) {
 
  
 
-// 1. Uppercase Conversion
-let str1 = "hello world";
-console.log("Uppercase:", str1.toUpperCase());
-
-// 2. Lowercase Conversion
-let str2 = "HELLO WORLD";
-console.log("Lowercase:", str2.toLowerCase());
-
-// 3. Find Word in Sentence
-str1="my name is prajwal"
-console.log(str1.includes("prajwal"))
-
-// 4. Find Word Position
-let sentence1 = "Hello world";
-console.log("Position of 'world':", sentence1.indexOf("world")); 
-
-// 5. Extract Substring
-let lang = "JavaScript";
-console.log("Extracted Substring:", lang.slice(4));
-function slice(a){
-    console.log(a.slice(4,10))
-}
-slice("javascript")
-
-// 6. Replace a Word
-let nameStr = "My name is John";
-console.log("After Replace:", nameStr.replace("John", "Prajwal"));
-
-// 7. Count String Length
-let message = "I love coding";
-console.log("Length:", message.length);
-
-// 8. Combine Two Strings
-let str3 = "Hello";
-let str4 = "World";
-console.log("Combined:",str3.concat(" ",str4));
-
-// 9. Check Start or End Word
-let str5 = "Hello JavaScript";
-console.log("Starts with 'Hello':", str5.slice(0,5));
-console.log("Ends with 'Script':", str5.slice(10,16));
-
-// 10. Reverse a String
-function reverseString(str) {
-   console.log(str.split("").reverse().join(""));
-}
-reverseString("Prajwal")
 
 
