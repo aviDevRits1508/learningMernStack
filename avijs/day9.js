4d25840b9fdcd22a68bcf221c1969f08e7042172
aviDevRits1508 <avinash.kumar@revappayyaitservices.com>
2025-10-21 12:09:14 +0530

    added

---
+++ avijs/day9.js
@@ -0,0 +1,50 @@
+//array of object full marks 20
+let student = [
+    {
+        name: "avinash",
+        class: "10",
+        address: "bangalore",
+        rollNumber: 1,
+        testMarks: 8
+    },
+    {
+        name: "ramesh",
+        class: "10",
+        address: "bangalore",
+        rollNumber: 2,
+        testMarks: 12
+    },
+    {
+        name: "suresh",
+        class: "10",
+        address: "bangalore",
+        rollNumber: 3,
+        testMarks: 9
+    }
+]
+
+
+//map
+// student.map((a) => {
+//     console.log(a)
+// })
+
+// let bonusMarks = student.map((a) => {
+//     return a.testMarks + 5
+// })
+
+// console.log(bonusMarks)
+
+
+
+// let names = student.map((s) => s.name.toUpperCase())
+// console.log(names)
+
+// let updatedMarks = student.map((s) => ({...s, testMarks: s.testMarks + 5}))
+
+// console.log(updatedMarks)
+
+
+// let summery = student.map((s) => `${s.name} has scored ${s.testMarks} marks`)
+
+// console.log(summery)
