99be8b058bfd8b3d9906d8fdb8b8ddda264543b2
Prerana.G <Prerana.G@revappayyaitservices.com>
2025-10-22 20:03:08 +0530

    array objects

---
+++ preranaPortfolio/preranajs/reduce.js
@@ -0,0 +1,14 @@
+// sum
+const numbers = [1, 2, 3, 4];
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0); 
+
+console.log(sum);
+ 
+// product
+const numbers2 = [5, 2, 3, 4];
+const product = numbers2.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+
+console.log(product);
+ 
+// max
+let numbers3 = [2,5,9,6,10];
